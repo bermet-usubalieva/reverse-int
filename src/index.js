f48987f5ad5b674c36a60b1e02df433d3855f643
Bermet <usubalievabermet@gmail.com>
2023-08-12 10:17:27 +0600

    feat: done reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let newNumber = Math.abs(n)
+    let str = String(newNumber);
+    let newStr = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[str.length - 1] == 0) {
+            newStr = newStr;
+        }
+        newStr = str[i] + newStr
+    }
+    return Number(newStr)
 }
